2db9f4b78d2abfc8734a457c528e7d47daea2477
toxa108 <antonskubiiev@gmail.com>
2019-12-23 16:30:03 +0700

    server init

---
+++ server/src/main/java/io/toxa108/blitzar/server/ServerApplication.java
@@ -0,0 +1,8 @@
+package io.toxa108.blitzar.server;
+
+import io.toxa108.blitzar.storage.StorageApplication;
+
+public class ServerApplication {
+    StorageApplication storageApplication = new StorageApplication();
+
+}
