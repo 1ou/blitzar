b57959b71015879d893b8d4870f846ad5cf1413a
toxa108 <antonskubiiev@gmail.com>
2020-02-28 17:40:50 +0700

    finals

--- storage/src/main/java/io/toxa108/blitzar/storage/database/manager/storage/btree/impl/BzDiskTreeReader.java
+++ storage/src/main/java/io/toxa108/blitzar/storage/database/manager/storage/btree/impl/BzDiskTreeReader.java
@@ -38,47 +38,40 @@ public class BzDiskTreeReader implements DiskTreeReader {
         this.pNonLeaf = tableTreeMetadata.entriesInNonLeafNodeNumber();
     }
 
-
-    private void checkNodeSize(final int size, final int availableSize) {
-        if (availableSize < size) {
-            throw new IllegalArgumentException("Node size can't be longer than size of disk page");
-        }
-    }
-
     @Override
     public TreeNode read(final int pos) throws IOException {
         log.info("Read from: " + pos);
-        Field primaryIndexField = tableTreeMetadata.primaryIndexField();
+        final Field primaryIndexField = tableTreeMetadata.primaryIndexField();
 
-        byte[] bytes = diskReader.read(pos, tableTreeMetadata.databaseConfiguration().diskPageSize());
-        boolean isLeaf = bytes[0] == 1;
-        byte[] amountOfEntriesBytes = new byte[Integer.BYTES];
+        final byte[] bytes = diskReader.read(pos, tableTreeMetadata.databaseConfiguration().diskPageSize());
+        final boolean isLeaf = bytes[0] == 1;
+        final byte[] amountOfEntriesBytes = new byte[Integer.BYTES];
         System.arraycopy(bytes, Byte.BYTES, amountOfEntriesBytes, 0, Integer.BYTES);
-        int amountOfEntries = BytesManipulator.bytesToInt(amountOfEntriesBytes);
+        final int amountOfEntries = BytesManipulator.bytesToInt(amountOfEntriesBytes);
 
         if (amountOfEntries == 0) {
             return new TreeNode(pos, new Key[pLeaf], new byte[pLeaf][tableTreeMetadata.dataSize()], true, 0, -1);
         }
 
         if (!isLeaf) {
-            Key[] keys = new Key[this.pNonLeaf];
-            int[] p = new int[this.pNonLeaf + 1];
+            final Key[] keys = new Key[this.pNonLeaf];
+            final int[] p = new int[this.pNonLeaf + 1];
 
-            byte[] entryPosBytes = new byte[Integer.BYTES];
-            byte[] tmpByteBuffer = new byte[Integer.BYTES];
+            final byte[] entryPosBytes = new byte[Integer.BYTES];
+            final byte[] tmpByteBuffer = new byte[Integer.BYTES];
             int sizeOfCurrentIndex;
 
             for (int i = 0; i < amountOfEntries; ++i) {
                 System.arraycopy(
                         bytes, tableTreeMetadata.reservedSpaceInNode() + Integer.BYTES * i, entryPosBytes, 0, Integer.BYTES);
 
-                int posOfIndex = BytesManipulator.bytesToInt(entryPosBytes) - pos;
+                final int posOfIndex = BytesManipulator.bytesToInt(entryPosBytes) - pos;
                 System.arraycopy(bytes, posOfIndex, tmpByteBuffer, 0, Integer.BYTES);
                 sizeOfCurrentIndex = BytesManipulator.bytesToInt(tmpByteBuffer);
 
                 System.arraycopy(bytes, posOfIndex + Integer.BYTES, tmpByteBuffer, 0, Integer.BYTES);
                 p[i] = BytesManipulator.bytesToInt(tmpByteBuffer);
-                byte[] currentIndexBytes = new byte[sizeOfCurrentIndex];
+                final byte[] currentIndexBytes = new byte[sizeOfCurrentIndex];
                 System.arraycopy(bytes, posOfIndex + Integer.BYTES * 2, currentIndexBytes, 0, sizeOfCurrentIndex);
                 keys[i] = new BzKey(new BzField(
                         primaryIndexField.name(),
@@ -103,28 +96,28 @@ public class BzDiskTreeReader implements DiskTreeReader {
                     -1
             );
         } else {
-            Key[] keys = new Key[this.pLeaf];
+            final Key[] keys = new Key[this.pLeaf];
 
-            byte[][] values = new byte[this.pLeaf][tableTreeMetadata.dataSize()];
+            final byte[][] values = new byte[this.pLeaf][tableTreeMetadata.dataSize()];
             int next = -1;
 
-            byte[] entryPosBytes = new byte[Integer.BYTES];
-            byte[] tmpByteBuffer = new byte[Integer.BYTES];
+            final byte[] entryPosBytes = new byte[Integer.BYTES];
+            final byte[] tmpByteBuffer = new byte[Integer.BYTES];
             int sizeOfCurrentIndex, sizeOfCurrentData;
 
             for (int i = 0; i < amountOfEntries; ++i) {
                 System.arraycopy(
                         bytes, tableTreeMetadata.reservedSpaceInNode() + Integer.BYTES * i, entryPosBytes, 0, Integer.BYTES);
 
-                int posOfIndex = BytesManipulator.bytesToInt(entryPosBytes) - pos;
+                final int posOfIndex = BytesManipulator.bytesToInt(entryPosBytes) - pos;
                 System.arraycopy(bytes, posOfIndex, tmpByteBuffer, 0, Integer.BYTES);
                 sizeOfCurrentIndex = BytesManipulator.bytesToInt(tmpByteBuffer);
 
                 System.arraycopy(bytes, posOfIndex + Integer.BYTES, tmpByteBuffer, 0, Integer.BYTES);
                 sizeOfCurrentData = BytesManipulator.bytesToInt(tmpByteBuffer);
 
-                byte[] currentIndexBytes = new byte[sizeOfCurrentIndex];
-                byte[] currentDataBytes = new byte[sizeOfCurrentData];
+                final byte[] currentIndexBytes = new byte[sizeOfCurrentIndex];
+                final byte[] currentDataBytes = new byte[sizeOfCurrentData];
                 System.arraycopy(bytes, posOfIndex + 2 * Integer.BYTES, currentIndexBytes, 0, sizeOfCurrentIndex);
                 System.arraycopy(bytes, posOfIndex + 2 * Integer.BYTES + sizeOfCurrentIndex,
                         currentDataBytes, 0, sizeOfCurrentData);
@@ -139,7 +132,7 @@ public class BzDiskTreeReader implements DiskTreeReader {
                 values[i] = currentDataBytes;
 
                 if (i == amountOfEntries - 1) {
-                    int nextLeafPos = posOfIndex + 2 * Integer.BYTES + sizeOfCurrentIndex + sizeOfCurrentData;
+                    final int nextLeafPos = posOfIndex + 2 * Integer.BYTES + sizeOfCurrentIndex + sizeOfCurrentData;
                     System.arraycopy(bytes, nextLeafPos, tmpByteBuffer, 0, Integer.BYTES);
                     next = BytesManipulator.bytesToInt(tmpByteBuffer);
                 }
