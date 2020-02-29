46d0221df72e3b4e3a4ec43a9cbecc6834211f5f
toxa108 <antonskubiiev@gmail.com>
2020-02-29 13:48:12 +0700

    disk writer io refactoring

--- storage/src/main/java/io/toxa108/blitzar/storage/io/impl/BzDiskWriterIo.java
+++ storage/src/main/java/io/toxa108/blitzar/storage/io/impl/BzDiskWriterIo.java
@@ -9,29 +9,20 @@ import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
- * Disk writer shouldn't know about all high level logic. It works only with bytes and files.
+ * Disk writer shouldn't know about all high level logic.
+ * It works only with bytes and files.
  */
 public class BzDiskWriterIo implements DiskWriter {
-    private final File file;
-    private final RandomAccessFile randomAccessFile;
-//    private final FileOutputStream fileOutputStream;
     private final FileChannel fileChannel;
 
     public BzDiskWriterIo(final File file) throws IOException {
-        this.file = file;
-        this.randomAccessFile = new RandomAccessFile(file, "rw");
+        final RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
         this.fileChannel = randomAccessFile.getChannel();
-//        this.fileOutputStream = new FileOutputStream(randomAccessFile.getFD());
     }
 
     @Override
     public void write(final int pos, final byte[] bytes) throws IOException {
-//        randomAccessFile.seek(pos);
-//        randomAccessFile.write(bytes);
-
-        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        final ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
         fileChannel.write(byteBuffer, pos);
-//        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
-//        bufferedOutputStream.write(bytes);
     }
 }
